7bf64c77102f8cf0279be194719bce40deacfb33
DamacenaBR <bruno23cz@gmail.com>
2020-06-11 20:52:56 -0300

    adicionando metodo inserir contatos

--- src/main/java/com/atividade/esii/agendatelefonica/controller/ContatoController.java
+++ src/main/java/com/atividade/esii/agendatelefonica/controller/ContatoController.java
@@ -1,8 +1,16 @@
 package com.atividade.esii.agendatelefonica.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.atividade.esii.agendatelefonica.model.Contato;
 import com.atividade.esii.agendatelefonica.repository.ContatoRepository;
 
 @Service
@@ -14,5 +22,11 @@ public class ContatoController {
 	public ContatoController(ContatoRepository repository) {
 		this.repository = repository;
 	}
+	
+	@PostMapping("/contatos")
+	public ResponseEntity<?> inserir(@RequestBody Contato contato){
+		return new ResponseEntity<>(repository.save(contato), HttpStatus.OK);
+	}
+	
 	
 }
