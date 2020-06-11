bfe7dfd99bdcc50d2716b948959d44281135d893
Gutemberg Ferreira Dantas <gutemberg.cz@gmail.com>
2020-06-11 13:18:46 -0300

    Adicionado ContatoController(rotas e método CRUD)

--- src/main/java/com/atividade/esii/agendatelefonica/controller/ContatoController.java
+++ src/main/java/com/atividade/esii/agendatelefonica/controller/ContatoController.java
@@ -1,5 +1,18 @@
 package com.atividade.esii.agendatelefonica.controller;
 
-public class ContatoController {
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.atividade.esii.agendatelefonica.repository.ContatoRepository;
+
+@Service
+@RestController
+public class ContatoController {
+	
+	private ContatoRepository repository;
+	
+	public ContatoController(ContatoRepository repository) {
+		this.repository = repository;
+	}
+	
 }
